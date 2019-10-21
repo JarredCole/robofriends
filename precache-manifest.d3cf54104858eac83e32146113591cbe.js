d20a7d853a33d657986a79fa6785d7ba394b30e0
Jarred Cole <dj.cole710@gmail.com>
2019-10-20 22:08:38 -0400

    Updates

---
+++ precache-manifest.d3cf54104858eac83e32146113591cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd2b616d45d91b005544cb59ca10764",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "525e29b4007d084c38df",
+    "url": "/robofriends/static/css/main.0d22bf76.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "525e29b4007d084c38df",
+    "url": "/robofriends/static/js/main.88dba8eb.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
